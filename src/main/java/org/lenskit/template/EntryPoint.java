b14116b2e80b0f04909fdeb4a8086594528179e5
Michael Ekstrand <md@ekstrandom.net>
2016-01-27 17:13:33 -0600

    Initial commit of a template project.

---
+++ src/main/java/org/lenskit/template/EntryPoint.java
@@ -0,0 +1,14 @@
+package org.lenskit.template;
+
+/**
+ * A <tt>main</tt> class that you can run.
+ */
+public class EntryPoint {
+    /**
+     * The main method where your program starts running.
+     * @param args The command line arguments, if any, received by your program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
